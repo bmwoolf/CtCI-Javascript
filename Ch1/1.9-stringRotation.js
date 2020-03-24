5db761e7bc53dc50d346c46d56d6d419781f0cb8
bmwoolf <bradleymwoolf@gmail.com>
2020-03-24 11:33:47 -0700

    string rotation with includes and doubling of larger string

---
+++ Ch1/1.9-stringRotation.js
@@ -0,0 +1,16 @@
+// find the point of rotation- basically if one of the strings is a substring of another
+
+function isRotation(str1, str2) {
+    let str1Length = str1.length;
+
+    // check that they are not empty strings
+    if (str1Length == str2.length && str1Length > 0) {
+        // basically double the longer string and see if it includes the smaller one- like in the cs challenge
+        let str1Double = str1 + str1;
+        return str1Double.includes(str2);
+    }
+    return false;
+}
+
+console.log(isRotation('waterbottle', 'erbottlewat')); // true
+console.log(isRotation('waterbottle', 'terbottwale')); // false
